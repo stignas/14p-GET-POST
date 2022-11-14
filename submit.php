fb9ff8824cbc1d004dc35bb1254a743ceecfacf1
Ignas <statkus.online@gmail.com>
2022-11-14 20:12:17 +0200

    minor update

--- submit.php
+++ submit.php
@@ -16,32 +16,34 @@ if (empty($taskArray)) {
     $taskArray = [];
 }
 $message = 'Error occured. Check your inputs and try again.';
+$newTask = $_GET['todo'];
+$duedatetime = $_GET['date'] . ' ' . $_GET['time'];
 ?>
 <!--# ---------------- Patikrinam ar visi laukai įvesti ------------------------------>
 
-<?php if ($_GET['todo'] != '' && $_GET['date'] != '' && $_GET['time'] != ''): ?>
+<?php if ($newTask != '' && $duedatetime != ''): ?>
 
-    <!--# ---------------- Įrašom naujus įvestus duomenis į masyvą ir įrašome į JSON duomenų failą -------------------->
+    <!--# ---------------- Įrašom naujus įvestus duomenis į masyvą -------------------->
     <?php
 
-    $duedatetime = $_GET['date'] . ' ' . $_GET['time'];
     $createdAt = date('Y-m-d H:i');
     $taskArray[] = [
-        'todo' => $_GET['todo'],
+        'todo' => $newTask,
         'createdAt' => $createdAt,
         "duedatetime" => $duedatetime
     ];
-
     ?>
-    <!-- Patikrinam ar 'due date' yra ateityje-->
+
+    <!-- Patikrinam ar 'due date' yra ateityje ir atnaujinam duomenų failą-->
+
     <?php if ($createdAt < $duedatetime): ?>
         <?php file_put_contents('data.json', json_encode($taskArray, JSON_PRETTY_PRINT));
 //        <!-- Pranešam apie sėkmingai įrašytą naują užduotį į failą -->
-        $message = 'Task <i>' . $_GET['todo'] . '</i> created.' ?>
+        $message = 'Task <i>' . $newTask . '</i> created.' ?>
     <?php endif ?>
 <?php endif ?>
-    <!-- Jeigu forma nepilnai užpildyta, pranešame apie klaidą ir įrašo į JSON failą nedarome.-->
-    <h2> <?= $message ?></h2>
+<!-- Jeigu forma nepilnai užpildyta, pranešame apie klaidą ir įrašo į JSON failą nedarome.-->
+<h2> <?= $message ?></h2>
 <!-- Redirectinam po 2s į pradinį puslapį -->
 <script>
     setTimeout(() => {
